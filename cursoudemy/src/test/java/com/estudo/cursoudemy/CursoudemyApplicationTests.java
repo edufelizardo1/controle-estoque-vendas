e0b5f93f98a863838b144435dafb0160ebaee1b0
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-17 19:51:53 -0300

    Create Project

---
+++ cursoudemy/src/test/java/com/estudo/cursoudemy/CursoudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.cursoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
