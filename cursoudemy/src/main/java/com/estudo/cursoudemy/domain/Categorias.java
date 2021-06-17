97360af2b5e6a198d75b064d9f21715424bf0cb7
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-17 20:28:21 -0300

    Create CatgoriaDomain

---
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/domain/Categorias.java
@@ -0,0 +1,55 @@
+package com.estudo.cursoudemy.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Categorias implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    public Categorias() {
+    }
+
+    public Categorias(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categorias)) return false;
+        Categorias that = (Categorias) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
