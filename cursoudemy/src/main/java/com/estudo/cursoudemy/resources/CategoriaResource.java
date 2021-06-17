610734d5ecca74e7d816b5230bb8371a4b6ad236
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-17 20:36:13 -0300

    Create Teste CatgoriaDomain

--- cursoudemy/src/main/java/com/estudo/cursoudemy/resources/CategoriaResource.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.estudo.cursoudemy.resources;
 
+import com.estudo.cursoudemy.domain.Categorias;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "REST está funcionando";
+    public List<Categorias> listar() {
+
+        Categorias cat1 = new Categorias(1, "Informatica");
+        Categorias cat2 = new Categorias(2, "Escritório");
+
+        List<Categorias> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 }
