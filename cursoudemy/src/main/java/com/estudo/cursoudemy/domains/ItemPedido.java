f178c04b0d14d44ba76fc790ab87994a566e720e
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 19:53:32 -0300

    Create ItemPedido.java

---
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/domains/ItemPedido.java
@@ -0,0 +1,101 @@
+package com.estudo.cursoudemy.domains;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "item_pedido")
+public class ItemPedido implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonIgnore
+    @EmbeddedId
+    private ItemPdidoPK id =new ItemPdidoPK();
+
+    private Double desconto;
+    private Integer quantidade;
+    private Double preco;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(Pedido pedido,Produto produto, Double desconto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public double getSubTotal() {
+        return (preco - desconto) * quantidade;
+    }
+
+    public Pedido getPedido() {
+        return id.getPedido();
+    }
+
+    public Produto getProduto(){
+        return  id.getProduto();
+    }
+
+    public ItemPdidoPK getId() {
+        return id;
+    }
+
+    public void setId(ItemPdidoPK id) {
+        this.id = id;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ItemPedido)) return false;
+        ItemPedido that = (ItemPedido) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "ItemPedido{" +
+                "id=" + id +
+                ", desconto=" + desconto +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco +
+                '}';
+    }
+}
