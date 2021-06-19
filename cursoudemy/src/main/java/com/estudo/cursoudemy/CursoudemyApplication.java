c795c4bcb5b11d8ab758809c8df8ef6132b46551
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 12:21:14 -0300

    Update CursoudemyApplication.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/CursoudemyApplication.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/CursoudemyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoudemyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursoudemyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CursoudemyApplication.class, args);
+    }
 
 }
