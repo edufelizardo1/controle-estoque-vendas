fc8ba980428329f5901e4176368fbe4a10522320
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 19:51:37 -0300

    Upadate Pedido.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/domains/Pedido.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/domains/Pedido.java
@@ -1,9 +1,14 @@
 package com.estudo.cursoudemy.domains;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "pedido")
 public class Pedido implements Serializable {
@@ -12,6 +17,8 @@ public class Pedido implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern="dd/MM/yyyy HH:mm")
     private Date instante;
 
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
@@ -22,8 +29,11 @@ public class Pedido implements Serializable {
     private Cliente cliente;
 
     @ManyToOne
-    @JoinColumn(name = "endereco_de-entrega_id")
+    @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itemPedidos = new HashSet<>();
 
     public Pedido() {
     }
@@ -33,6 +43,14 @@ public class Pedido implements Serializable {
         this.instante = instante;
         this.cliente = cliente;
         this.enderecoDeEntrega = enderecoDeEntrega;
+    }
+
+    public double getValorTotal() {
+        double soma = 0.0;
+        for (ItemPedido ip : itemPedidos) {
+            soma = soma + ip.getSubTotal();
+        }
+        return soma;
     }
 
     public Integer getId() {
@@ -75,6 +93,14 @@ public class Pedido implements Serializable {
         this.enderecoDeEntrega = enderecoDeEntrega;
     }
 
+    public Set<ItemPedido> getItemPedidos() {
+        return itemPedidos;
+    }
+
+    public void setItemPedidos(Set<ItemPedido> itemPedidos) {
+        this.itemPedidos = itemPedidos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -86,5 +112,17 @@ public class Pedido implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", instante=" + instante +
+                ", pagamento=" + pagamento +
+                ", cliente=" + cliente +
+                ", enderecoDeEntrega=" + enderecoDeEntrega +
+                ", itemPedidos=" + itemPedidos +
+                '}';
     }
 }
