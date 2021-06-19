63044a4d7cd66e9241b639ca3f93a2017c370209
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 12:18:08 -0300

    Update CategoriaRepository.java

---
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.cursoudemy.repositories;
+
+import com.estudo.cursoudemy.domains.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
