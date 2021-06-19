0b847f2588ee4fed910e8c4763d2b617cb34ed71
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 19:52:30 -0300

    Upadate PagamentoComBoleto.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/domains/PagamentoComBoleto.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/domains/PagamentoComBoleto.java
@@ -1,14 +1,20 @@
 package com.estudo.cursoudemy.domains;
 
 import com.estudo.cursoudemy.domains.enumeration.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import javax.persistence.Entity;
 import java.util.Date;
 @Entity
+@JsonTypeName(value = "pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento{
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date dataVencimento;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date datapagamento;
 
     public PagamentoComBoleto(){
