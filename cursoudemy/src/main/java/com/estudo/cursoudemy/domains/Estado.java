6ce00706a3a11a434009c46242b6f94d9d09e153
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 17:45:54 -0300

    Update Estado.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/domains/Estado.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/domains/Estado.java
@@ -1,5 +1,7 @@
 package com.estudo.cursoudemy.domains;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ public class Estado implements Serializable {
     private Integer id;
     private String nome;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades = new ArrayList<>();
 
