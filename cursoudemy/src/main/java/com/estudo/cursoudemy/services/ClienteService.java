e48d0b61f8ff7d41f7e2c4783c3e94d2af1b906b
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 17:22:14 -0300

    Update ClienteService.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/services/ClienteService.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/services/ClienteService.java
@@ -2,6 +2,7 @@ package com.estudo.cursoudemy.services;
 
 import com.estudo.cursoudemy.domains.Cliente;
 import com.estudo.cursoudemy.repositories.ClienteRepository;
+import com.estudo.cursoudemy.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,8 @@ public class ClienteService {
 
     public Optional<Cliente> listarId (Integer id) {
         Optional<Cliente> cliente = clienteRepository.findById(id);
-        return cliente;
+        return Optional.ofNullable(cliente.orElseThrow(()-> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
+        )));
     }
 }
