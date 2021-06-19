2d4d8591898be19d89d87b215605f6b3be804710
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 16:46:40 -0300

    Create EnderecoRepository.java

---
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.cursoudemy.repositories;
+
+import com.estudo.cursoudemy.domains.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+}
