29a63e83ce859b696394c80b16b6878dff8c94b9
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-19 12:23:27 -0300

    Update ObjectNotFoundException.java

--- cursoudemy/src/main/java/com/estudo/cursoudemy/services/exception/ObjectNotFoundException.java
+++ cursoudemy/src/main/java/com/estudo/cursoudemy/services/exception/ObjectNotFoundException.java
@@ -1,7 +1,7 @@
 package com.estudo.cursoudemy.services.exception;
 
 public class ObjectNotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 3149600940123334641L;
+    private static final long serialVersionUID = 1L;
 
     public ObjectNotFoundException(String msg) {
         super(msg);
